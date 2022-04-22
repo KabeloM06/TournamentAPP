6f5dfb82477d0da30bd789facee0b7631b844178
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-22 13:05:55 +0200

    End of day save

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,19 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
+
+        
     {
+        private const string PrizesFile = "PrizeModels.csv";
         // TODO - Wire up the CreatePrize for text files
 
         public PrizeModel CreatePrize(PrizeModel model)
         {
             // Load the text file
             //Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
             // Find the max ID
+            // order the list by ID in descending order then increament ID for new ID
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+
             // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+            
+            
+            
+            
             // Convert the prizesto list<string>
             // Save the List<strng> to the text file
         }
